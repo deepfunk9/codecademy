c8ca5b18139536bd5c4e8cfbc1926adc6db13d50
deepfunk9 <56848514+deepfunk9@users.noreply.github.com>
2019-12-31 12:56:10 +0000

    Stu attempt

--- project guessing game/script.js
+++ project guessing game/script.js
@@ -4,3 +4,28 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const computerGuess = () => {
+    return Math.floor(Math.random() * 9);
+}
+
+const humanGuess = 3;
+
+console.log('Auto: ' + generateTarget());
+
+console.log('Computer: ' + computerGuess());
+
+console.log('Human: ' + humanGuess);
+
+const compareGuesses = (humanGuess, computerGuess, generateTarget) => {
+    if (humanGuess === generateTarget) {
+        return console.log('Match');
+    } else if (Math.abs(humanGuess - generateTarget) < Math.abs(computerGuess - generateTarget)) {
+        return true;
+    } else {return false;}
+}
+
+console.log(compareGuesses());
